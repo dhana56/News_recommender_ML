85dac8bf1f3b58e84d74e99d92db537e392d5395
dhana <dhanasekharvk@gmail.com>
2023-12-15 20:02:55 +0530

    changes final

--- Bot 2/Collaborative_FIltering 23.py
+++ Bot 2/Collaborative_FIltering 23.py
@@ -1,35 +1,23 @@
 import numpy as np
 import warnings
 import pandas as pd
-from numpy import dot
-from numpy.linalg import norm
 from sklearn.metrics.pairwise import cosine_similarity
 warnings.filterwarnings("ignore")
 
-
-
 news = pd.read_csv("MegaZord.csv")
-
 
 #randomly generating ratings
 trial_user_rating_df = pd.DataFrame([[np.random.randint(0, 6) for i in range(len(news))] for user in range(50)], columns = news.index)
 
-
 #random user chosen
 user_id = np.random.randint(0,50)
 #print(user_id)
-
-
 #dummy variable recommend to compare ratings
 recommend = pd.DataFrame(trial_user_rating_df)
-
-
 
 #finding 5 closest neighbours of chosen user
 cos_sim = pd.DataFrame(cosine_similarity(recommend))
 neighbours = pd.DataFrame(cos_sim[user_id].sort_values(ascending=False)[1:6])
-
-
 
 #changing the rating of viewed articles to 0 and unviewed articles according to weightage of and rating given by neighbours 
 def new_func(trial_user_rating_df, user_id, neighbours, article, article_score, rating):
@@ -44,15 +32,12 @@ for article in recommend.columns:
     else:
         recommend.loc[user_id,article] = 0
         
-
 #sorting recommend row of user according to ratings 
 recco = []
 for i in recommend.columns:
-    recco.append(recommend[i][user_id])
-    
+    recco.append(recommend[i][user_id])   
 recco = pd.DataFrame(recco)
 recco = recco.sort_values(by = [0], ascending = False)[:10]
-
 
 #appending news headline and link to empty dataframe output
 output = pd.DataFrame(columns = ['Headline','News_Link'])
@@ -61,7 +46,4 @@ for i in recco.index:
     link = news.iloc[i]['News_Link']#[user_id]
     output = output.append({'Headline' : headline, 'News_Link' : link},ignore_index = True)
 output.index+=1
-
-
-
 print(output)
--- Bot 2/content_based.py
+++ Bot 2/content_based.py
@@ -1,5 +1,4 @@
-'''importing the required libraries '''
-    
+'''importing the required libraries '''   
 import pandas as pd
 import numpy as np 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -8,19 +7,10 @@ import BOT1
 
 # making the dataframe using the numpy module 
 Dataframe = pd.read_csv("MegaZord.csv")
-
-# printing the first 5 entries 
-#print(Dataframe.head())
-# making the dataframe using the numpy module 
-#Dataframe = pd.read_csv("C:\\Users\\dhana\\OneDrive\\Desktop\\Conjoined.csv")
 Dataframe.drop_duplicates(subset ="News_Link",
                     keep = False, inplace = True, ignore_index= True)
 
-
-
 ''' Using the Tf-idf vectorizer to conver the text file into the vector file '''
-
-
 TFIDF= TfidfVectorizer(stop_words='english')
 
 #converting the the texts into corresponding unicode values 
@@ -28,20 +18,12 @@ corpus = Dataframe[' cleaned_news'].values.astype('U')
 
 #converted unicodes are passing into the the tf-idf vectorizer 
 X = TFIDF.fit_transform(corpus)
-X
-
 
 ''' next  we are implementing the cosine similarity matrix based on the X vexctor by using the linear_kernel '''
 cosinesimilarity = linear_kernel(X, X)
 
 # printing the indices of the cleaned news entries
 indices = pd.Series(Dataframe.index, index= Dataframe['Headline']).drop_duplicates()
-##indices
-
-#checking the indice variable gives the correct values for the news headings that we are providing  
-#indices ['"Just Priceless": Shamita Shetty Shares Adorable Video With Shilpa Shetty And Their Mother']
-
-
 
 # buliding the recommendation system function which  news that are based on the cosine similarity values 
 def get_recommendation(titile, cosinesimilarity = cosinesimilarity):
@@ -50,16 +32,11 @@ def get_recommendation(titile, cosinesimilarity = cosinesimilarity):
     cos_scores = enumerate(cosinesimilarity[idx])
     cos_scores =sorted(cos_scores, key = lambda x: x[1], reverse= True)
     cos_scores =cos_scores[1:11]
-    
     for i in cos_scores:
-    # print (i)
         cos_index = [i[0] for i in cos_scores]
-            #  print(cos_index)
     print(Dataframe[['Headline','News_Link']].iloc[cos_index])
-    #print(Dataframe[['News_Link']].iloc[cos_index])
-#print(number_gen)
-# entering the news and giving the recommendation based on the news headline that we are provided 
 
+# entering the news and giving the recommendation based on the news headline that we are provided 
 number_gen = np.random.randint(0, 10)
 k = BOT1.bot1['Headline'][ number_gen]
 get_recommendation(k)
